9d5bde54beb54999976d1501b2ec04a479b485cd
Fabrizio Catinella <2322021c@student.gla.ac.uk>
2020-04-02 00:11:35 +0100

    Validation: Login and Sign Up Users

--- lector_app/urls.py
+++ lector_app/urls.py
@@ -7,7 +7,6 @@ app_name = 'lector-app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
     path('signup/', views.signup, name='signup'),
     path('uploads/', views.uploads, name='uploads'),
     path('library/', views.library, name='library'),
@@ -18,4 +17,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('book_search/', views.book_search, name='book_search'),
     path('audio_player/', views.audio_player, name='audio_player'),
+    path('validate_login/', views.validate_login, name='validate_login'),
+    path('validate_signup/', views.validate_signup, name='validate_signup'),
 ]
