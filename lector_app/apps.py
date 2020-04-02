acc2d6c009b0cd7196a8f283ea0db487738c4584
Paolo Lammens <2475444L@student.gla.ac.uk>
2020-04-02 13:00:32 +0200

    fix: broaden database error catch

--- lector_app/apps.py
+++ lector_app/apps.py
@@ -1,7 +1,8 @@
 import logging
 
 from django.apps import AppConfig
-from django.db.utils import OperationalError
+from django.db.migrations.exceptions import InvalidBasesError
+from django.db.utils import DatabaseError
 
 
 class LectorAppConfig(AppConfig):
@@ -14,6 +15,6 @@ class LectorAppConfig(AppConfig):
             from .models import Recording
             Recording.indexer.init_index()
             Recording.indexer.reindex_all()
-        except OperationalError as error:
-            logging.exception(f"OperationalError while loading {self.label} (try migrating first)",
+        except (DatabaseError, InvalidBasesError) as error:
+            logging.exception(f"database error while loading {self.label} (try migrating first)",
                               exc_info=error)
