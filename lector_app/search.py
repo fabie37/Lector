bb33a1a7cf88ea7550f70113e23726a4f693fab0
Paolo Lammens <2475444L@student.gla.ac.uk>
2020-04-02 12:42:30 +0200

    fix: use AsyncWriter instead of BufferedWriter

--- lector_app/search.py
+++ lector_app/search.py
@@ -1,44 +1,41 @@
 import typing as t
 
 import whoosh.fields
-import whoosh.writing
 from django.conf import settings
 from django.db.models import Model
 from whoosh.fields import Schema
 from whoosh.index import Index, create_in
+from whoosh.writing import AsyncWriter
 
 from .utils import mkdir, pre_call_hook
 
+PK_FIELDTYPE = whoosh.fields.ID(stored=True, unique=True)
 
-class AbstractSearchFunctionality:
-    PK_FIELDTYPE = whoosh.fields.ID(stored=True, unique=True)
 
+class AbstractIndexer:
     # fields (instance attributes):
     model: t.Type[Model]
     schema: Schema
     index: Index
-    name: str
 
     def __init__(self, model: t.Type[Model], schema: Schema,
-                 pk_name: str = 'id', name: t.Optional[str] = None):
+                 pk_name: str = 'id', index_name: t.Optional[str] = None):
         """
         :param model: django.db.models.Model subclass whose instances are to be searched
         :param schema: field schema for the search index
-        :param name: name of the search index
+        :param index_name: name of the search index
         :param pk_name: field name of the model's primary key
         """
         self.model = model
         self.schema = schema
-        self.schema.add(pk_name, AbstractSearchFunctionality.PK_FIELDTYPE)
+        self.schema.add(pk_name, PK_FIELDTYPE)
         self.pk_name = pk_name
-        self.name = name
-        self.index = self._create_index()
-        self._buffered_writer = whoosh.writing.BufferedWriter(self.index)
+        self.index_name = index_name
 
-    def _create_index(self) -> Index:
+    def init_index(self) -> Index:
         """Initialise the empty index"""
         mkdir(settings.SEARCH_INDEX_DIR)
-        self.index = create_in(settings.SEARCH_INDEX_DIR, self.schema, indexname=self.name)
+        self.index = create_in(settings.SEARCH_INDEX_DIR, self.schema, indexname=self.index_name)
         return self.index
 
     def _check_instance(self, instance: Model):
@@ -59,48 +56,24 @@ class AbstractSearchFunctionality:
 
     @pre_call_hook(_check_instance)
     def index(self, instance: Model):
-        """Add an entry to the index"""
+        """Add an entry to the index. Non-blocking."""
         return self.reindex(instance)
 
     @pre_call_hook(_check_instance)
     def reindex(self, instance: Model):
-        """Update an entry in the index"""
-        self._buffered_writer.update_document(**self.extract_search_fields(instance))
+        """Update an entry in the index. Non-blocking."""
+        with AsyncWriter(self.index) as writer:
+            writer.update_document(**self.extract_search_fields(instance))
 
     @pre_call_hook(_check_instance)
     def remove(self, instance: Model):
-        """Remove an entry from the index"""
-        self._buffered_writer.delete_by_term(self.pk_name, instance.id)
+        """Remove an entry from the index. Non-blocking."""
+        with AsyncWriter(self.index) as writer:
+            writer.delete_by_term(self.pk_name, instance.id)
 
-    def reindex_all(self):
-        """Reset the index and index all instances of ``self.model``"""
-        with self._buffered_writer as writer:
+    def reindex_all(self, timeout=0.5):
+        """Reset the index and index all instances of ``self.model``. Blocking operation."""
+        with self.index.writer(timeout=timeout) as writer:
             for instance in self.model.objects.all():
                 writer.add_document(**self.extract_search_fields(instance))
             writer.mergetype = whoosh.writing.CLEAR
-
-    def __del__(self):
-        self._buffered_writer.close()
-
-
-class RecordingSearch(AbstractSearchFunctionality):
-    from .models import Recording
-
-    model = Recording
-    schema = Schema(book_title=whoosh.fields.TEXT(spelling=True),
-                    author_name=whoosh.fields.TEXT(spelling=True),
-                    reader_name=whoosh.fields.TEXT(spelling=True))
-
-    def __init__(self, name: t.Optional[str] = None):
-        super().__init__(RecordingSearch.model, RecordingSearch.schema, name=name)
-
-    def extract_search_fields(self, recording: Recording) -> t.Dict[str, str]:
-        super().extract_search_fields(recording)
-        book, author, reader = recording.book, recording.book.author, recording.reader
-        fields = dict(book_title=book.title,
-                      author_name=author.full_name,
-                      reader_name=reader.full_name)
-        return {k: str(v) for k, v in fields.items()}
-
-
-RECORDING_SEARCH = RecordingSearch()
