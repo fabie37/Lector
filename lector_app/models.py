932bcbf89ad4675fea56fd9b051f3e574f0fb69e
Paolo Lammens <2475444L@student.gla.ac.uk>
2020-04-03 18:45:58 +0200

    fix: remove unused languages field

--- lector_app/models.py
+++ lector_app/models.py
@@ -1,49 +1,11 @@
 import typing as t
 
-import language_tags
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.base import ModelBase
-from langcodes import Language
 
 from . import search
 from .utils import HasHumanName
-
-
-# ----- Custom fields -----
-
-def validate_language_tag(value: str):
-    if not language_tags.tags.check(value):
-        raise ValidationError(f'invalid language tag: {value}')
-
-
-class LanguageField(models.Field):
-    """
-    Field representing a natural language variant. Stored on the database as a BCP-47 language
-    tag (a CharField, thus), and as a :class:`langcodes.Lanuage` object for its Python
-    representation.
-    """
-
-    def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 16
-        kwargs['validators'] = [validate_language_tag]
-        super().__init__(*args, **kwargs)
-
-    def get_internal_type(self):
-        return 'CharField'
-
-    def from_db_value(self, db_value: str, expression, connection) -> Language:
-        return self.to_python(db_value)
-
-    def to_python(self, value: t.Union[Language, str, None]) -> t.Optional[Language]:
-        if value is None or isinstance(value, Language):
-            return value
-        # `value` is a language tag; parse it:
-        return Language.get(value)
-
-    def get_prep_value(self, value: Language) -> str:
-        return value.to_tag()
 
 
 # ----- Abstract Models & metaclasses -----
